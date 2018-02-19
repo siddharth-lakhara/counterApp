f52ff872121137d26fdfc12c1fa4fda0851c92cc
lakhara <siddharth.lakhara@gmail.com>
2018-02-19 10:55:05 +0530

    Boiler Plate

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from './button';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello <br />
+        <Button />
+      </div>
+    );
+  }
+}
+
+export default App;
