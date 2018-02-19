9fc344dfb4803caff6bcacbd15c4603bd32be481
lakhara <siddharth.lakhara@gmail.com>
2018-02-19 11:15:02 +0530

    Added reducer for count

---
+++ src/redux/reducers/couter.js
@@ -0,0 +1,17 @@
+
+const defaultState = {
+  count: 0,
+};
+
+const counter = (state = defaultState, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { ...state.count, count: state.count + 1 };
+    case 'DECREMENT':
+      return { ...state.count, count: state.count - 1 };
+    default:
+      return { state };
+  }
+};
+
+export default counter;
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,8 @@
+import { combinedReducers } from 'redux';
+import couter from './couter';
+
+combinedReducers({
+  counter,
+});
+
+export default combinedReducers;
